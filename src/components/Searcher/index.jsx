401f11945f6026e416dc259ea180d6a47c1ca04f
roiber21 <roiberale@gmail.com>
2022-08-05 22:34:18 -0500

    Colocando los estilos del buscador(searcher)

--- src/components/Searcher/index.jsx
+++ src/components/Searcher/index.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import { IconButton, Stack, TextField } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Searcher = () =>{
     return (
-        <Stack>
-        <TextField />
-        <IconButton />
+        <Stack
+        direction='row'
+        sx={{
+            marginTop:'30px',
+            width:'80%'
+        }}>
+        <TextField
+        id="outlined-basic" 
+        placeholder='Octocat'
+        label="Github User" 
+        variant="outlined"
+        size='small'  
+        sx={{
+            width:'90%',
+        }}
+        />
+       <IconButton 
+       size='small'
+       sx={{
+        left: '-45px'
+       }}>
+        <SearchIcon/>
+       </IconButton>
     </Stack> 
     )
    
