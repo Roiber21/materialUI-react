193e667247f5ac00811c698c4f869ecf3d8988f2
roiber21 <roiberale@gmail.com>
2022-08-25 18:04:23 -0500

    Ajuste de estilos con SX a los componentes de descripción e información principal

--- src/containers/userscards/index.jsx
+++ src/containers/userscards/index.jsx
@@ -1,4 +1,4 @@
-import { CardMedia, Grid } from '@mui/material';
+import { CardMedia, Grid, Stack } from '@mui/material';
 import React from 'react'
 import PrincipalInformation from '../../components/PrincipalInformation';
 import Description from '../Description';
@@ -8,18 +8,35 @@ const UserCard = (props) => {
     const { avatar_url }= userState;
 
     return (
-        <Grid container spacing={2}> 
+        <Grid 
+        container 
+        spacing={2}
+        sx={{marginTop:'15px'}}
+        > 
       <Grid item xs={3}> 
             <CardMedia
             component='img'
             alt='github user'
             image={avatar_url}
+            sx={{
+                borderRadius:'50%',
+                marginLeft:'5px'
+            }}
             />
       </Grid>
       <Grid item xs={9}>
-      <PrincipalInformation userState={userState} />
-         </Grid>
+            <Stack
+              direction='column'
+              spacing={1} 
+            sx={{
+              margin:'30px',
+
+                
+                }}>
+            <PrincipalInformation userState={userState} />
             <Description userState={userState} />
+            </Stack>
+         </Grid>
         </Grid>
     )
 
